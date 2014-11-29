3d504ab8cda9ed92d3351653a2602dc167ae1cc5
George Ke <george-ke@hotmail.com>
2014-11-29 17:01:11 -0500

    Changed Dragon icon to D.

--- src/dragon.h
+++ src/dragon.h
@@ -12,7 +12,7 @@ class Dragon: public Enemy {
         DragonTreasure *treasure;
     public:
         static const std::string TYPE_NAME;
-        static const char TYPE_ID = 'R';
+        static const char TYPE_ID = 'D';
         static const int HP = 150;
         static const int ATK = 20;
         static const int DEF = 20;
